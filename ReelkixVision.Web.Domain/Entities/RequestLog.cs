5874ab4bec51c53a3f93666d8b943600dc5abddf
domidomdomz <domz.ciriaco@gmail.com>
2025-03-19 23:01:38 +0800

    Add Domain Entities

---
+++ ReelkixVision.Web.Domain/Entities/RequestLog.cs
@@ -0,0 +1,11 @@
+﻿namespace ReelkixVision.Web.Domain.Entities
+{
+    public class RequestLog
+    {
+        public int Id { get; set; }
+        public string? FileName { get; set; }
+        public string? Url { get; set; }
+        public string? ResponseDetails { get; set; }
+        public DateTime RequestTime { get; set; }
+    }
+}
