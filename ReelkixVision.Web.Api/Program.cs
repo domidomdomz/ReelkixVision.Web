0b1c42aa3195dcb130a642d5756a8e17f294067e
domidomdomz <domz.ciriaco@gmail.com>
2025-03-20 00:53:59 +0800

    Setup the Dependencies and Middleware

--- ReelkixVision.Web.Api/Program.cs
+++ ReelkixVision.Web.Api/Program.cs
@@ -1,6 +1,27 @@
+using Amazon.S3;
+using Microsoft.EntityFrameworkCore;
+using ReelkixVision.Web.Application.Interfaces;
+using ReelkixVision.Web.Infrastructure.AWS;
+using ReelkixVision.Web.Infrastructure.FeatureFlags;
+using ReelkixVision.Web.Infrastructure.Persistence;
+using ReelkixVision.Web.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add AWS S3 client via AWS SDK DI Extensions.
+builder.Services.AddAWSService<IAmazonS3>();
+
+// Register project services.
+builder.Services.AddScoped<IImageService, ImageService>();
+builder.Services.AddScoped<IFeatureFlagService, FeatureFlagService>();
+builder.Services.AddScoped<ILoggingService, LoggingService>();
+
+// Configure EF Core (using a free/local SQL Server instance, adjust as needed).
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
