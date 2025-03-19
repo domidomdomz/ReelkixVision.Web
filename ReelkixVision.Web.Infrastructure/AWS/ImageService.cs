3c59a3559c127ed0520fd93cb83ce6bdf205b8e1
domidomdomz <domz.ciriaco@gmail.com>
2025-03-19 23:20:42 +0800

    Add Image service implementation

---
+++ ReelkixVision.Web.Infrastructure/AWS/ImageService.cs
@@ -0,0 +1,39 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using ReelkixVision.Web.Application.Interfaces;
+
+namespace ReelkixVision.Web.Infrastructure.AWS
+{
+    public class ImageService : IImageService
+    {
+        private readonly IAmazonS3 _amazonS3;
+        private readonly IConfiguration _configuration;
+
+        public ImageService(IAmazonS3 amazonS3, IConfiguration configuration)
+        {
+            _amazonS3 = amazonS3;
+            _configuration = configuration;
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile file)
+        {
+            // Read bucket name and create a unique file name.
+            string bucketName = _configuration["AWS:BucketName"] ?? "";
+            string fileName = $"{Guid.NewGuid()}-{file.FileName}";
+
+            using var stream = file.OpenReadStream();
+            var request = new PutObjectRequest
+            {
+                BucketName = bucketName,
+                Key = fileName,
+                InputStream = stream,
+                ContentType = file.ContentType
+            };
+
+            await _amazonS3.PutObjectAsync(request);
+            return $"https://{bucketName}.s3.amazonaws.com/{fileName}";
+        }
+    }
+}
