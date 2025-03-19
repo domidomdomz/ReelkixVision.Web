e89370d42e0e172101131be8c502099c20ce0204
domidomdomz <domz.ciriaco@gmail.com>
2025-03-19 23:35:11 +0800

    Add Feature Flag Service

---
+++ ReelkixVision.Web.Infrastructure/FeatureFlags/FeatureFlagService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using ReelkixVision.Web.Application.Interfaces;
+
+namespace ReelkixVision.Web.Infrastructure.FeatureFlags
+{
+    public class FeatureFlagService : IFeatureFlagService
+    {
+        private readonly IConfiguration _configuration;
+
+        public FeatureFlagService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public bool IsDatabaseLoggingEnabled()
+        {
+            return bool.Parse(_configuration["FeatureFlags:DatabaseLogging"] ?? "false");
+        }
+    }
+}
